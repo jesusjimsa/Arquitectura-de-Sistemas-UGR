bcfb5b4f9d546b00ba98e276649dc5a12e35a45f
Jesús Jiménez Sánchez <jesusjimsa@icloud.com>
2019-02-25 16:19:48 +0100

    Intentando imprimir texto

--- Práctica 2/Ficheros/solucion/solucion.s
+++ Práctica 2/Ficheros/solucion/solucion.s
@@ -1,25 +1,26 @@
-.code16            # código de 16 bits
+.code16				# código de 16 bits
 
-.text              # sección de código
-	.globl _start  # punto de entrada
+.text				# sección de código
+	.globl _start	# punto de entrada
 
 _start:
-	xor %ax, %ax       # 0 --> ax   |
-	mov %ax, %ds       # ax --> ds  | msg --> ds:si
-	mov $msg, %si      # msg --> si |
+	xor %ax, %ax		# 0 --> ax   |
+	mov %ax, %ds		# ax --> ds  | msg --> ds:si
+	mov $msg, %si		# msg --> si |
 
 sig:
 	mov $0xe, %ah
-	mov %ds:(%si), %al
+	mov %ds:(%si), %al	# Caracter que se desea imprimir
 	xor %bh, %bh
 	movb $0x04, %bl		# Aquí debería cambiar el color de la letra, pero la BIOS no funciona bien
-	int $0x10
-	loop sig           # decrementa cx y salta si no 0
+	int $0x10			# Interrupción para imprimir por pantalla
+	inc %si
+	loop sig			# decrementa cx y salta si no 0
 	
 	cli
 	hlt
 
-msg: .ascii "Hola mundo"
+msg: .string "Hola mundo"
 fin:
-	.org 510       # posición 510
-	.word 0xAA55   # firma
+	.org 510		# posición 510
+	.word 0xAA55	# firma
