ddf456876be9c9e79e0511a1c7ea928d600a44f7
Jesús Jiménez Sánchez <jesusjimsa@icloud.com>
2019-03-01 12:20:22 +0100

    Ahora imprime la cadena y la empieza en la memoria de vídeo

--- Práctica 2/Ficheros/solucion/solucion.s
+++ Práctica 2/Ficheros/solucion/solucion.s
@@ -7,7 +7,13 @@ _start:
 	xor %ax, %ax		# 0 --> ax   |
 	mov %ax, %ds		# ax --> ds  | msg --> ds:si
 	mov $msg, %si		# msg --> si |
+	
+	movw $0xb800, %ax  # 0xb800 --> ax |
+	movw %ax, %es      # ax --> es     | video --> es:di
+	xorw %di, %di      # 0 --> di      |
 
+
+	mov tam, %cx
 sig:
 	mov $0xe, %ah
 	mov %ds:(%si), %al	# Caracter que se desea imprimir
@@ -16,11 +22,13 @@ sig:
 	int $0x10			# Interrupción para imprimir por pantalla
 	inc %si
 	loop sig			# decrementa cx y salta si no 0
-	
+
+fin:	
 	cli
 	hlt
 
 msg: .string "Hola mundo"
-fin:
+tam: .short . - msg
+
 	.org 510		# posición 510
 	.word 0xAA55	# firma
