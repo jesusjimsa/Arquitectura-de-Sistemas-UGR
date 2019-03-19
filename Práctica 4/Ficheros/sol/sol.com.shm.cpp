87e17fed496fa6d2be80ac33c731d906bc29937d
Jesús Jiménez Sánchez <jesusjimsa@icloud.com>
2019-03-19 13:04:20 +0100

    shm

--- Práctica 4/Ficheros/sol/sol.com.shm.cpp
+++ Práctica 4/Ficheros/sol/sol.com.shm.cpp
@@ -9,5 +9,31 @@
 using namespace std;
 
 int main(){
-    
+    int key = ftok("tmp/com.shm", 3);
+	int shmid = shmget(key, 100, 0644 | IPC_CREAT);
+	char *data = nullptr;
+
+	switch(fork()){
+		case -1:
+			cout << "Fallo en fork()!";
+
+			break;
+		case 0:
+			data = static_cast<char*>(shmat(shmid, nullptr, 0));
+			data = "mensajito";
+
+			break;
+		default:
+			data = static_cast<char*>(shmat(shmid, nullptr, 0));
+			
+			cout << "Recibido por el padre: " << data << endl;
+			
+			break;
+	}
+
+	/* detach from the segment: */
+    if (shmdt(data) == -1) {
+        perror("shmdt");
+        exit(1);
+    }
 }
