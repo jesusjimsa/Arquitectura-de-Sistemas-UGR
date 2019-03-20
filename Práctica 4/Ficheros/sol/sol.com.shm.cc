2cae166241bcd953dffaabb31f5174c8a7d1ba77
Jesús Jiménez Sánchez <jesusjimsa@icloud.com>
2019-03-20 23:16:29 +0100

    Envía cosas

--- Práctica 4/Ficheros/sol/sol.com.shm.cc
+++ Práctica 4/Ficheros/sol/sol.com.shm.cc
@@ -9,8 +9,8 @@
 using namespace std;
 
 int main(){
-    int key = ftok("tmp/com.shm", 3);
-	int shmid = shmget(key, 100, 0600 | IPC_CREAT);
+    int key = ftok("/tmp/sol.com.shm", 3);
+	int shmid = shmget(key, 100, IPC_CREAT | 0600);
 	char *data = nullptr;
 
 	switch(fork()){
@@ -20,10 +20,13 @@ int main(){
 			break;
 		case 0:
 			data = static_cast<char*>(shmat(shmid, nullptr, 0));
-			data = "mensajito";
+			
+			*data = 'p';
 
 			break;
 		default:
+			wait(nullptr);	// Espera a que el hijo acabe su ejecución
+
 			data = static_cast<char*>(shmat(shmid, nullptr, 0));
 			
 			cout << "Recibido por el padre: " << data << endl;
