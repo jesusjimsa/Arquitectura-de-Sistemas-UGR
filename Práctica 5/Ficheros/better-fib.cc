4b4c6e35627e277130421f0f816a4d812fd9772f
Jesús Jiménez Sánchez <jesusjimsa@icloud.com>
2019-03-21 18:51:08 +0100

    fib mejorado

---
+++ Práctica 5/Ficheros/better-fib.cc
@@ -0,0 +1,33 @@
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+#include <thread>
+
+using namespace std;
+
+template<class T> T fib(T n){
+	if (n < 2){
+		return n;
+	}
+	else{
+		return thread(fib, n - 1) + thread(fib, n - 2);
+	}
+}
+
+int main(int argc, char *argv[]){
+	if (argc < 2){
+		throw invalid_argument("necesito un número como parámetro");
+	}
+
+	istringstream iss(argv[1]);
+	
+	unsigned long long n;
+	iss >> n;
+	
+	if (!iss){
+		throw invalid_argument("el parámetro no es un número válido");
+	}
+
+	cout << argv[0] << "(" << argv[1] << ") = " << fib(n) << endl;
+}
