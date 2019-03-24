9078127619d8430631fd9c4d0dd02b34dca83711
Jesús Jiménez Sánchez <jesusjimsa@icloud.com>
2019-03-24 17:56:48 +0100

    mq

--- Práctica 4/ping-pong/ping.pong.mq.cc
+++ Práctica 4/ping-pong/ping.pong.mq.cc
@@ -3,16 +3,100 @@
 //---------------------------------------------------------
 
 #include <iostream>
+#include <fcntl.h>           /* For O_* constants */
+#include <sys/stat.h>        /* For mode constants */
+#include <mqueue.h>
+
+#define QUEUE_NAME  "/ping_pong_queue"
+#define MAX_SIZE    1024
 
 using namespace std;
+
+const char PING = '1', PONG = '0';
+int ping = 0, pong = 0;
+
+//---------------------------------------------------------
+
+void show_ping(int){
+	cout << "ping = " << ping << endl;
+
+	exit(EXIT_SUCCESS);
+}
+
+//---------------------------------------------------------
+
+void show_pong(int){
+	cout << "pong = " << pong << endl;
+
+	exit(EXIT_SUCCESS);
+}
+
+//---------------------------------------------------------
 
 //---------------------------------------------------------
 
 int main(){
-	int ping = 0, pong = 0;
+	mqd_t mq;
+	 struct mq_attr attr;
+	 char c;
 
-	cout << "ping = " << ping << endl
-	    << "pong = " << pong << endl;
+	switch(fork()){
+		case -1:
+			cerr << "Fallo en fork()" << endl;
+			break;
+		case 0:	// Hijo
+			signal(SIGALRM, show_pong);
+			alarm(1);
+
+			/* initialize the queue attributes */
+			attr.mq_flags = 0;
+			attr.mq_maxmsg = 10;
+			attr.mq_msgsize = sizeof(char);
+			attr.mq_curmsgs = 0;
+
+			/* create the message queue */
+    		mq = mq_open(QUEUE_NAME, O_CREAT | O_RDWR | O_NONBLOCK, 0644, &attr);
+
+			while(true){
+				mq_send(mq, PONG, sizeof(char), 0);
+
+				do{
+					mq_receive(mq, c, sizeof(char), NULL);
+				}while(c == PONG);
+
+				pong++;
+			}
+
+			break;
+		default:	// Padre
+			signal(SIGALRM, show_ping);
+			alarm(1);
+
+			/* initialize the queue attributes */
+			attr.mq_flags = 0;
+			attr.mq_maxmsg = 10;
+			attr.mq_msgsize = sizeof(char);
+			attr.mq_curmsgs = 0;
+
+			/* create the message queue */
+    		mq = mq_open(QUEUE_NAME, O_CREAT | O_RDWR | O_NONBLOCK, 0644, &attr);
+
+			while(true){
+				mq_send(mq, PING, sizeof(char), 0);
+
+				do{
+					mq_receive(mq, c, sizeof(char), NULL);
+				}while(c == PING);
+
+				ping++;
+			}
+
+			break;
+	}
+
+	/* cleanup */
+    mq_close(mq);
+    mq_unlink(QUEUE_NAME);
 }
 
 //---------------------------------------------------------
