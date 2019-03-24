8d895a15044319e98832e152cf517d56b11068b4
Jesús Jiménez Sánchez <jesusjimsa@icloud.com>
2019-03-24 21:52:00 +0100

    Estilo

--- Práctica 5/Ficheros/ackermann.cc
+++ Práctica 5/Ficheros/ackermann.cc
@@ -1,16 +1,23 @@
 #include <iostream>
 
-template<typename T> T ackermann(T m, T n)
-{
-	if (m == 0) return n + 1;
-	if (n == 0) return ackermann(m - 1, 1);
+using namespace std;
+
+template<typename T> T ackermann(T m, T n){
+	if (m == 0){
+		return n + 1;
+	}
+
+	if (n == 0){
+		return ackermann(m - 1, 1);
+	}
+	
 	return ackermann(m - 1, ackermann(m , n - 1));
 }
 
-int main()
-{
-	for (int m = 0; m < 4; ++m)
-		for (int n = 0; n < 13; ++n)
-			std::cout << "ackermann(" << m << ", " << n << ") = " 
-			          << ackermann(m, n) << std::endl;
+int main(){
+	for (int m = 0; m < 4; ++m){
+		for (int n = 0; n < 13; ++n){
+			cout << "ackermann(" << m << ", " << n << ") = " << ackermann(m, n) << endl;
+		}
+	}
 }
