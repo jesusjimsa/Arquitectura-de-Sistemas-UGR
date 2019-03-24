2e9753f4f4e64717c22ba1a689405bb3d921f339
Jesús Jiménez Sánchez <jesusjimsa@icloud.com>
2019-03-24 18:09:12 +0100

    Back

--- Práctica 4/ping-pong/ping.pong.mq.cc
+++ Práctica 4/ping-pong/ping.pong.mq.cc
@@ -39,19 +39,6 @@ void show_pong(int){
 
 //---------------------------------------------------------
 
-void initialize_mq(mqd_t *mq, struct mq_attr *attr){
-	/* initialize the queue attributes */
-	attr.mq_flags = 0;
-	attr.mq_maxmsg = 10;
-	attr.mq_msgsize = sizeof(char);
-	attr.mq_curmsgs = 0;
-
-	/* create the message queue */
-	mq = mq_open(QUEUE_NAME, O_CREAT | O_RDWR | O_NONBLOCK, 0644, &attr);
-}
-
-//---------------------------------------------------------
-
 int main(){
 	mqd_t mq;
 	struct mq_attr attr;
@@ -65,7 +52,14 @@ int main(){
 			signal(SIGALRM, show_pong);
 			alarm(1);
 
-			initialize_mq(&mq, &attr);
+			/* initialize the queue attributes */
+			attr.mq_flags = 0;
+			attr.mq_maxmsg = 10;
+			attr.mq_msgsize = sizeof(char);
+			attr.mq_curmsgs = 0;
+
+			/* create the message queue */
+    		mq = mq_open(QUEUE_NAME, O_CREAT | O_RDWR | O_NONBLOCK, 0644, &attr);
 
 			while(true){
 				mq_send(mq, &PONG, sizeof(char), 0);
@@ -86,7 +80,14 @@ int main(){
 			signal(SIGALRM, show_ping);
 			alarm(1);
 
-			initialize_mq(&mq, &attr);
+			/* initialize the queue attributes */
+			attr.mq_flags = 0;
+			attr.mq_maxmsg = 10;
+			attr.mq_msgsize = sizeof(char);
+			attr.mq_curmsgs = 0;
+
+			/* create the message queue */
+    		mq = mq_open(QUEUE_NAME, O_CREAT | O_RDWR | O_NONBLOCK, 0644, &attr);
 
 			while(true){
 				mq_send(mq, &PING, sizeof(char), 0);
