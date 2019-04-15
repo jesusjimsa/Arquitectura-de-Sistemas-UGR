df99a9c4eb6a5b394b6a923251010f460992a844
Jesús Jiménez Sánchez <jesusjimsa@icloud.com>
2019-04-15 10:41:11 +0200

    Signals arreglado y funcionando

--- Práctica 8/Ficheros/signals.cc
+++ Práctica 8/Ficheros/signals.cc
@@ -11,29 +11,35 @@ thread c, s;
 
 void cliente(int){
 	signal(SIGUSR2, cliente);
+
 	cout << "[cliente ]: respuesta recibida\n";
 	pthread_kill(s.native_handle(), SIGUSR1);
 	cout << "[cliente ]: petición enviada\n";
-	this_thread::sleep_for(1s);
 }
 
 void servidor(int){
 	signal(SIGUSR1, servidor);
+	
 	cout << "[servidor]: petición recibida\n";
 	pthread_kill(c.native_handle(), SIGUSR2);
 	cout << "[servidor]: respuesta enviada\n";
-	this_thread::sleep_for(1s);
 }
 
 int main(){
 	alarm(1);
 
-	signal(SIGUSR2, cliente);
-	signal(SIGUSR1, servidor);
+	s = thread([]{
+		signal(SIGUSR1, servidor);
+		
+		c = thread([]{
+			signal(SIGUSR2, cliente);
+			cout << "[cliente ]: petición enviada" << endl;
+			pthread_kill(s.native_handle(), SIGUSR1);
+			this_thread::sleep_for(1s);
+		});
 
-	c = thread(cliente, SIGUSR2);
-	s = thread(servidor, SIGUSR1);
+		c.join();
+	});
 
-	c.join();
 	s.join();
 }
